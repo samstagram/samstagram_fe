43ef65e905e2fee41e7cbb3b0e01f9cfe5a94a4b
GEUNA <geuna0204@gmail.com>
2022-08-20 04:22:09 +0900

    [add] add Button element

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,8 @@
-// export const colors = {
-//   black: '#1A1A1A',
-//   white: '#FFFFFF',
-//   gray1: '#333333',
-//   gray2: '#8A8A8A',
-//   gray3: '#D9D9D9',
-//   darkBlue: '#002D3A',
-//   blue: '#186EDD',
-//   red: '#D7382C',
-//   yellow: '#EFB70E',
-//   green1: '#28A745',
-//   green2: '#94DCA9',
-// };
+export const colors = {
+  black: '#262626',
+  white: '#FFFFFF',
+  gray1: '#8A8A8A',
+  gray2: '#DBDBDB',
+  blue: '#0095F6',
+  red: '#ED4956',
+};
