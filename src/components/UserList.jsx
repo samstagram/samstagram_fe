b164a851e24e082a81e3883e59603b3b39bf69f9
GEUNA <geuna0204@gmail.com>
2022-08-21 07:13:35 +0900

    [add] userlist scrollbar 커스터마이징

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -6,12 +6,26 @@ const UserList = () => {
   return (
     <StUserList>
       <h2>회원님을 위한 추천</h2>
-      <ul>
-        <UserItem />
-        <UserItem />
-        <UserItem />
-        <UserItem />
-      </ul>
+      <div>
+        <ul className="scroll scrollbar">
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+          <UserItem />
+        </ul>
+      </div>
     </StUserList>
   );
 };
@@ -19,9 +33,36 @@ const UserList = () => {
 export default UserList;
 
 const StUserList = styled.div`
+  .scroll {
+    display: inline-block;
+    width: 320px;
+    height: 600px;
+    padding: 12px;
+    overflow-y: scroll;
+    border: 1px solid ${colors.gray2};
+    border-radius: 6px;
+    box-sizing: border-box;
+  }
+
+  .scrollbar::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  // scrollbar style
+  .scrollbar::-webkit-scrollbar-thumb {
+    background-color: ${colors.gray2};
+    border-radius: 10px;
+  }
+
+  // scrollbar background
+  .scrollbar::-webkit-scrollbar-track {
+    background-color: ${colors.gray_bg};
+  }
+
   h2 {
     color: ${colors.gray1};
     font-size: 16px;
     font-weight: 500;
+    margin-bottom: 10px;
   }
 `;
