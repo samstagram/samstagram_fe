de2aadb1d8f9ec5c1fb698cf57ad3cd377d5f6f1
GEUNA <geuna0204@gmail.com>
2022-08-21 07:13:17 +0900

    modal 배경 스크롤 방지

--- src/components/layout/Modal.jsx
+++ src/components/layout/Modal.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { colors } from "styles/theme";
 
 const Modal = ({ handleOpenModal, children }) => {
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
   return (
     <StModal onClick={handleOpenModal}>
       <StModalBody onClick={(e) => e.stopPropagation()}>{children}</StModalBody>
