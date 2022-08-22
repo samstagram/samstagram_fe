7d44905ad3aadbf014ae03d71e46a231c170197a
GEUNA <geuna0204@gmail.com>
2022-08-23 03:48:13 +0900

    [add] 구글 로그인 및 회원 라우팅 기능 구현

---
+++ src/components/GoogleLogin.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "shared/api";
+import axios from "axios";
+import { setCookie } from "shared/cookie";
+import { __getUsers } from "redux/modules/usersSlice";
+import { useDispatch } from "react-redux";
+
+const GoogleLogin = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  // 인가코드
+  let code = new URL(window.location.href).searchParams.get("code");
+
+  useEffect(() => {
+    if (code) {
+      const google = async () => {
+        try {
+          const res = await axios.get(
+            `${BASE_URL}/api/oauth/google/callback?code=${code}`
+          );
+          console.log(res);
+          (await res.headers.authorization) &&
+            setCookie("mycookie", res.headers.authorization);
+          await dispatch(__getUsers());
+
+          navigate("/");
+        } catch (err) {
+          console.log("소셜로그인 에러", err);
+          window.alert("로그인에 실패하였습니다.");
+        }
+      };
+      google();
+    }
+  }, []);
+
+  return <div></div>;
+};
+
+export default GoogleLogin;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,31 +2,10 @@ import styled from "styled-components";
 import samstagram_logo from "assets/samstagram_logo.png";
 import Button from "components/elements/Button";
 import { colors } from "styles/theme";
-import { Link } from "react-router-dom";
-import { useEffect } from "react";
-import axios from "axios";
+import { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } from "shared/api";
 
 const Login = () => {
-  const GOOGLE_CLIENT_ID =
-    "877084231575-p8uv6t4s185vln40vhsdab86gnviqurq.apps.googleusercontent.com";
-  const GOOGLE_REDIRECT_URL =
-    "http://sparta-09-tm.shop/api/oauth/google/callback";
-  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URL}&response_type=code&scope=email%20profile%20openid&access_type=offline`;
-
-  let code = new URL(window.location.href).searchParams.get("code");
-
-  useEffect(() => {
-    if (code) {
-      const google = () => {
-        const res = axios.get(
-          `http://sparta-09-tm.shop/oauth/google/callback?code=${code}`
-        );
-        console.log(res);
-      };
-
-      google();
-    }
-  }, [code]);
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email%20profile%20openid&access_type=offline`;
 
   return (
     <StLoginContainer>
@@ -35,13 +14,7 @@ const Login = () => {
       </ImageContainer>
       <Button variant="google">
         <span>
-          <a
-            href={GOOGLE_AUTH_URL}
-            // href="http://sparta-09-tm.shop/oauth2/authorization/google"
-            target="_blank"
-          >
-            Google로 로그인
-          </a>
+          <a href={GOOGLE_AUTH_URL}>Google로 로그인</a>
         </span>
       </Button>
       <MessageContainer>
@@ -62,7 +35,7 @@ const StLoginContainer = styled.div`
   margin: 160px auto;
   padding: 60px 0;
   background-color: ${colors.white};
-  border: 1px solid ${colors.gray1};
+  border: 1px solid ${colors.gray2};
   border-radius: 4px;
   gap: 40px;
   position: relative;
@@ -84,7 +57,7 @@ const StLoginContainer = styled.div`
 const ImageContainer = styled.div`
   width: 180px;
   padding-bottom: 40px;
-  border-bottom: 1px solid ${colors.gray1};
+  border-bottom: 1px solid ${colors.gray2};
 
   img {
     width: 100%;
